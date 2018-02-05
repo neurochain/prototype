f7d9279995599985de1292ad3d719047e6baff22
NeuroChain dev <neurochaintech@gmail.com>
2018-02-05 23:18:01 +0100

    Optimize enthalpy

--- experimentation/enthalpy.js
+++ experimentation/enthalpy.js
@@ -1,7 +1,10 @@
-var paramBots =25000;
-var paramBlocks = 2000;
-var paramTrxPerBlock = 10000;
-var paramTop =3;
+// node --max-old-space-size=8192 enthalpy.js
+
+var paramBots = 25000;
+var paramBlocks = 4000;
+var paramTrxPerBlock = 15000;
+
+var paramTop = 3;
 
 var arr = [];
 function set() {
@@ -9,7 +12,6 @@ function set() {
   for(var botsCount = 1; botsCount <paramBots; botsCount++){
     bots.push(botsCount);
   }
-
   for(var i= 0; i<paramBlocks; i++) {
       var block ={};
       block.blockId = i;
@@ -27,34 +29,46 @@ function set() {
       }
       arr.push(block);
   }
-
 }
 set();
 var top = [];
-var enthalpy= new Map();
+var enthalpy = {};
 var start = Date.now();
-// Enthalpy is sum of transaction by bots
-arr.map(function(obj){
+//enthalpy : Somme des valeurs
+var valByBotsSyBlocks = arr.map(function(obj){
+  var rObj = {};
   obj.content.forEach(function(trx) {
-    enthalpy.set(trx.emitterId, (enthalpy.get(trx.emitterId) || 0) + trx.value );
+    enthalpy[trx.emitterId] = (enthalpy[trx.emitterId] || 0) + trx.value;
   });
+  return rObj;
 });
-
 var stop = Date.now();
 
 var startSort =Date.now();
-var arrE = Array.from(enthalpy);
-arrE.sort(function(a, b){
-  return b[1] -a[1];
-});
-var stopSort =Date.now();
+var arrE = [];
+Object.entries(enthalpy).forEach(
+  (entry) => {
+    //console.log(arrE.length, entry);
+    if (arrE.length < paramTop) {
+      arrE.push(entry);
+      // sort arrE
+    } else {
+      for (var j=0;j<arrE.length;j++) {
+        if (entry[1] > arrE[j][1]) {
+          arrE[j] = entry;
+          // sort arrE
+          return;
+        }
+      }
+    }
+  }
+);
+var stopSort = Date.now();
 
-
-
-console.log("Total Transactions : " + paramBlocks*paramTrxPerBlock);
+console.log("Total Transactions : "+  paramBots*paramTrxPerBlock);
 console.log('enthalpy process time (ms) ' + parseInt(stop-start));
 console.log('sort process time (ms) ' + parseInt(stopSort-startSort));
-console.log('Top ' + paramTop);
-for(var i = 0; i<paramTop; i++){
+console.log('Top ');
+for(var i = 0; i<arrE.length; i++){
   console.log(arrE[i]);
 }
